af98aad036a254092c66088385319bbd871438e7
BagrKonstantin <57901534+BagrKonstantin@users.noreply.github.com>
2022-11-26 23:16:21 +0300

    Create foo.c

---
+++ part1/foo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+double f(double x, double a, double b) {
+    return a + b * x * x * x;
+}
+
+int main() {
+    double l, r, a, b;
+    int n;
+    double res = 0.0;
+    scanf( "%lf", &a );
+    scanf( "%lf", &b );
+    scanf( "%lf", &l );
+    scanf( "%lf", &r );
+    scanf("%d", &n);
+
+    double h = double((r - l) / n);
+    for (double x = l; x <= r; x += h) {
+        res += f(x - h / 2, a, b);
+    }
+
+    res *= h;
+    printf( "%f", res );
+    return 0;
+}
